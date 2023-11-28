7309fcc9c99df3a3ad883ed1d390874f44a0939b
Liams-theCreator <mmeellaallii1@gmail.com>
2023-11-28 18:26:42 +0100

    Fizzazi

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,7 +5,7 @@ def fizzbuzz():
             print("Fizz", end=" ")
         elif i % 5 == 0:
             print("Buzz", end=" ")
-        elif i % 3 == 0 and i % 5 == 0:
+        elif i % 15 == 0:
             print("FizzBuzz", end=" ")
         else:
             print("{:d}".format(i), end=" ")

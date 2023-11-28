01b247546ab282325ff208dff72ce16d73529911
Liams-theCreator <mmeellaallii1@gmail.com>
2023-11-28 19:27:05 +0100

    Updated

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,6 +2,4 @@
 for a in range(90, 64, -1):
     if a % 2 == 0:
         a = a + 32
-        print("{:c}".format(a), end="")
-    else:
-        print("{:c}".format(a), end="")
+        print("{:c}".format(chr(a)), end="")

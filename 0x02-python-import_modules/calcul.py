0b233ff80f73dbe0a280786b513757b7778707b1
Liams-theCreator <mmeellaallii1@gmail.com>
2023-11-30 22:02:16 +0100

    Calcul functions

---
+++ 0x02-python-import_modules/calcul.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+#addition
+def add(a, b):
+    return (a + b)
+#substraction
+def sub(a, b):
+    return (a - b)
+#multiplication
+def mul(a, b):
+    return (a * b)
+#division
+def div(a, b):
+    return int(a / b)

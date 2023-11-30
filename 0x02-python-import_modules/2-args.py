85eeeaf31651ba7fae01550e7c18f2a3fbb7a4e2
Liams-theCreator <mmeellaallii1@gmail.com>
2023-11-30 19:44:49 +0100

    Test

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    length = len(argv)
+    if length == 0:
+        print("0 argument.")
+    elif length == 1:
+        for i in range:
+            print("{:d} argument:".format(length))
+            print("{:d}: {:c}".format(length, argv[a]))
+    else:
+        for a in range(length):
+            print("{:d} argument:".format(length))
+            print("{:d}: {:c}".format(length, argv[a]))

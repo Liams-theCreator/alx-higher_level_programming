f8c52b3f8301b445c01d6e852904cc287f691142
Liams-theCreator <mmeellaallii1@gmail.com>
2023-11-30 21:23:14 +0100

    Python is deblij of curse

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    list = dir(hidden_4)
-    for i in list:
+    lista = dir(hidden_4)
+    for i in lista:
         if not i.startswith('_'):
+            list.sort(lista)
             print("{:s}".format(i))

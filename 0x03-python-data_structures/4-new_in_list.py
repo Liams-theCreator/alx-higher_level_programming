83c6dbf6a41f3841d4bc71070f08e468d37ec629
Liams-theCreator <mmeellaallii1@gmail.com>
2023-12-02 17:45:36 +0100

    Add element at index

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,6 @@ def new_in_list(my_list, idx, element):
     newlist = [i for i in my_list]
     length = len(newlist)
     if idx >= length or idx < 0:
-        return None
+        return newlist
     newlist[idx] = element
     return newlist

7d1ee8d25d46388e03d94303b8509545c528ba46
Liams-theCreator <mmeellaallii1@gmail.com>
2023-12-02 19:19:44 +0100

    Delete at index

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-# not done yet
+def delete_at(my_list=[], idx=0):
+    if my_list is None or idx is None or idx >= len(my_list):
+        return None
+    del my_list[idx]
+    return my_list

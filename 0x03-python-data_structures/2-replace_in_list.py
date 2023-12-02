ce551e3151d5a7c66e62d80a0eeda8255bd57a27
Liams-theCreator <mmeellaallii1@gmail.com>
2023-12-02 17:58:32 +0100

    Replace at index

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,6 +2,6 @@
 def replace_in_list(my_list, idx, element):
     length = len(my_list)
     if idx >= length or idx < 0:
-        return None
+        return my_list
     my_list[idx] = element
     return my_list

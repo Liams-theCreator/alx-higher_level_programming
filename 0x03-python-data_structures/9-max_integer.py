a69573db35a7cd95d6be6fe220a6cb591dfec198
Liams-theCreator <mmeellaallii1@gmail.com>
2023-12-03 16:34:55 +0100

    Max in list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-# not done yet
+def max_integer(my_list=[]):
+    if my_list is None:
+        return None
+    maxpayne = my_list[0]
+    for i in my_list:
+        if i > maxpayne:
+            maxpayne = i
+    return maxpayne

de2a6e639d1c1f2acfe191f0598d23d84f79f024
Liams-theCreator <mmeellaallii1@gmail.com>
2023-12-03 15:45:09 +0100

    Print matrixmen

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix is None:
         return None
+    idx = 0
     for rows in matrix:
-        for element in rows:
-            print("{:d}".format(element), end=" ")
-        print()
+        for idx, elements in enumerate(rows):
+            print("{:d}".format(elements), end=" ")
+    print()

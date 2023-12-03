6f5a3fddcfb17b14840910f8dbf4db040dee6d3f
Liams-theCreator <mmeellaallii1@gmail.com>
2023-12-03 16:25:33 +0100

    Multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None:
-        sentence[0] = None
+    if sentence is "":
+        return (0, None)
     tupile = (len(sentence), sentence[0])
     return tupile

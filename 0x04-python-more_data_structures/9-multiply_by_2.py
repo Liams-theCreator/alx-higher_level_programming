f834284f74dfabff966764289ae28c55cf7bba82
Liams-theCreator <mmeellaallii1@gmail.com>
2023-12-06 19:44:50 +0100

    Get me back to C

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    newdictio = dict()
+    for name, value in a_dictionary:
+        val = value * 2
+        newdictio[name] = val
+    return newdictio

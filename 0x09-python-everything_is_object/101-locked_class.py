72db6aaa681b9776f2c16f0a21f5a1c7296ce530
Liams-theCreator <mmeellaallii1@gmail.com>
2024-01-05 18:43:19 +0100

    Low memory cost

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" This file containts a class
+"""
+
+
+class LockedClass:
+    """ Class create object LockedClass"""
+    __slots__ = ['first_name']
+
+    def __init__(self):
+        """ Method - Inicializate """
+        pass

c1b5200647cda75f822ce8e725221a7d7e964a78
Liams-theCreator <mmeellaallii1@gmail.com>
2024-03-11 17:53:27 +0000

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,9 @@
 const argu = process.argv.length;
 
 if (argu === 2) {
-  console.log("No argument");
+  console.log('No argument');
+} else if (argu === 3) {
+  console.log('Argument found');
 } else {
-  console.log("Argument found");
+  console.log('Argument found');
 }

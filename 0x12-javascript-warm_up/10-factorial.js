a5a623b19e5e1ef2050f022b76d181b537728f96
Liams-theCreator <mmeellaallii1@gmail.com>
2024-03-11 21:14:54 +0000

    Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const Argu = parseInt(process.argv[2]);
+
+function factorial (number) {
+  if (number === 1 || !number) {
+    return (1);
+  }
+  return (number * factorial(number - 1));
+}
+
+console.log(factorial(Argu));

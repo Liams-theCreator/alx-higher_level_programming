69064b7a180f97757baecad052396f864082842a
Liams-theCreator <mmeellaallii1@gmail.com>
2024-03-11 20:53:02 +0000

    Addition Function

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+function add(a, b) {
+    return a + b;
+}
+
+const argu1 = parseInt(process.argv[2]);
+const argu2 = parseInt(process.argv[3]);
+
+console.log(add(argu1, argu2));

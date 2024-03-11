d20ee957ea001067275593f197ab9db25b977116
Liams-theCreator <mmeellaallii1@gmail.com>
2024-03-11 17:48:20 +0000

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-let argu = process.argv.length;
+const argu = process.argv.length;
 
 if (argu <= 2) {
-  console.log('No argument');
+  console.log("No argument");
 } else {
-  console.log('Argument found');
+  console.log("Argument found");
 }

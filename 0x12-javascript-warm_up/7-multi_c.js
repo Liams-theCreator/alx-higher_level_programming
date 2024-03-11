467c4173e9a65f8b44272ee08b319693ec694a48
Liams-theCreator <mmeellaallii1@gmail.com>
2024-03-11 20:33:21 +0000

    I think I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,6 +4,6 @@ const argu = process.argv[2];
 if (!argu) {
   console.log('Missing number of occurrences');
 }
-for (let i = 0; i <= argu; i++) {
+for (let i = 0; i < argu; i++) {
   console.log('C is fun');
 }

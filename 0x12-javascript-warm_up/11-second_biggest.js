5d66c5da89b421c1a3fe855578b2fb8d7bb0a40e
Liams-theCreator <mmeellaallii1@gmail.com>
2024-03-11 21:43:54 +0000

    Second Biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,6 @@
 if (process.argv.length <= 3) {
   console.log('0');
 } else {
-  const list = process.argv.sort();
-  console.log(list.reverse()[1]);
+  const list = process.argv.slice(2).sort((a, b) => b - a);
+  console.log(list[1]);
 }

8c328fc28548c997eb0eb34bfeab340cffe66e5a
Liams-theCreator <mmeellaallii1@gmail.com>
2024-03-11 20:45:28 +0000

    Square pants

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const size = parseInt(process.argv[2]);
+
+if (!size) 
+{
+    console.log('Missing size');
+}
+for (let i = 0; i < size; i++)
+{
+    console.log('X'.repeat(size))
+}
